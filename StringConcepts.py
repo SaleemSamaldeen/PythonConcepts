a597a73194e95c0c4e7790f4fafe1b40278a99fe
saleemsamaldeen <saleemsamaldeen@gmail.com>
2025-03-26 22:13:54 +0100

    String test updated

--- StringConcepts.py
+++ StringConcepts.py
@@ -6,7 +6,7 @@ count = len(name)   #to find the length of the string
 upperCase = name.upper() #to convert uppercase
 lowCase = name.lower()  #to convert lowercase
 firstLetterCapital = name.capitalize() # to capitalize only first letter
-leftStrip = name.lstrip("Python ") #left strip from given textfgjhfjfgjgk
+leftStrip = name.lstrip("Python ") #left strip from given text
 
 print(leftStrip)
 print(result)
