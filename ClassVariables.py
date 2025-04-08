a9ce3f3b78419a050efef97c27cf12eff001ae6e
saleem <saleemsamaldeen@gmail.com>
2025-04-08 11:56:27 +0200

    class objects

--- ClassVariables.py
+++ ClassVariables.py
@@ -1,17 +1,30 @@
 class Employee:
 
     experience = 11   #class variables. also known as static variables
+    age = 0
 
-    def __init__(self):
+    def __init__(self):  #Constructor in python always start with __init__
         self.number = 5001    #instance variables
         self.designation = "Software analyst"   #instance variables
+
+    def setAge(self,age):
+        self.age = age
+
+    def getAge(self):
+        return self.age
+
+    def config(self):
+        print(self.number, self.designation)
 
 employee = Employee()
 juniorEmp = Employee()
 
 employee.number = 5412
 juniorEmp.number = 1234
-Employee.experience = 5  #class variables can be accessed via class itself and affect also values in objects
+Employee.experience = 5  #class variables can be accessed via class itself and also affect values in objects
+employee.setAge(45) #encapsulation
 
 print(employee.number, employee.designation, employee.experience)
 print(juniorEmp.number, juniorEmp.designation, juniorEmp.experience)
+juniorEmp.config()
+print(employee.getAge(), juniorEmp.getAge())
